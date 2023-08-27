175356b8284ee89a9215df7fdb82964e0b8c9a59
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-27 13:05:38 +0300

    add core animation view and button

--- Lesson3.01/ViewController.swift
+++ Lesson3.01/ViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var coreAnimationView: UIView!
+    
+    @IBAction func startCoreAnimation(_ sender: UIButton) {
     }
-
-
 }
 

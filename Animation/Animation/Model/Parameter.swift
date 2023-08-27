ce9b9f72e620398beaa8bda5feceb418d4c65a6e
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-08-27 23:13:38 +0300

    add directories

---
+++ Animation/Animation/Model/Parameter.swift
@@ -0,0 +1,12 @@
+//
+//  Parameter.swift
+//  Animation
+//
+//  Created by Goodwasp on 27.08.2023.
+//
+
+import Foundation
+
+struct Parameter {
+    
+}
---
+++ Animation/Animation/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Goodwasp on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
